36dca31d152359bb07359688b3afeca75643a84f
Quentin Laffont <quentin@flexper.fr>
2022-04-19 09:09:40 +0200

    feat(app): first commit

---
+++ src/index.ts
@@ -0,0 +1,364 @@
+/* eslint-disable no-empty */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import '@formatjs/intl-displaynames/polyfill';
+import '@formatjs/intl-listformat/polyfill-force';
+import '@formatjs/intl-numberformat/polyfill';
+import '@formatjs/intl-pluralrules/polyfill';
+
+import { NumberFormatOptions } from '@formatjs/ecma402-abstract';
+import { DisplayNamesOptions } from '@formatjs/intl-displaynames';
+import { IntlListFormatOptions } from '@formatjs/intl-listformat';
+import {
+  format,
+  formatDistance,
+  formatDistanceToNow,
+  formatDuration,
+  formatRelative,
+  Locale,
+} from 'date-fns';
+import * as dateFNSLocaleFiles from 'date-fns/locale';
+import rosetta from 'rosetta';
+
+interface Language {
+  dict: Record<string, unknown>;
+  locale: Locale;
+}
+
+interface Locales {
+  af: Locale;
+  ar: Locale;
+  arDZ: Locale;
+  arEG: Locale;
+  arMA: Locale;
+  arSA: Locale;
+  arTN: Locale;
+  az: Locale;
+  be: Locale;
+  bg: Locale;
+  bn: Locale;
+  bs: Locale;
+  ca: Locale;
+  cs: Locale;
+
+  cy: Locale;
+
+  da: Locale;
+
+  de: Locale;
+
+  deAT: Locale;
+
+  el: Locale;
+
+  enAU: Locale;
+
+  enCA: Locale;
+
+  enGB: Locale;
+
+  enIE: Locale;
+
+  enIN: Locale;
+
+  enNZ: Locale;
+
+  enUS: Locale;
+
+  enZA: Locale;
+
+  eo: Locale;
+
+  es: Locale;
+
+  et: Locale;
+
+  eu: Locale;
+
+  faIR: Locale;
+
+  fi: Locale;
+
+  fil: Locale;
+
+  fr: Locale;
+
+  frCA: Locale;
+
+  frCH: Locale;
+
+  fy: Locale;
+
+  gd: Locale;
+
+  gl: Locale;
+
+  gu: Locale;
+
+  he: Locale;
+
+  hi: Locale;
+
+  hr: Locale;
+
+  ht: Locale;
+
+  hu: Locale;
+
+  hy: Locale;
+
+  id: Locale;
+
+  is: Locale;
+
+  it: Locale;
+
+  ja: Locale;
+
+  jaHira: Locale;
+
+  ka: Locale;
+
+  kk: Locale;
+
+  km: Locale;
+
+  kn: Locale;
+
+  ko: Locale;
+
+  lb: Locale;
+
+  lt: Locale;
+
+  lv: Locale;
+
+  mk: Locale;
+
+  mn: Locale;
+
+  ms: Locale;
+
+  mt: Locale;
+
+  nb: Locale;
+
+  nl: Locale;
+
+  nlBE: Locale;
+
+  nn: Locale;
+
+  pl: Locale;
+
+  pt: Locale;
+
+  ptBR: Locale;
+
+  ro: Locale;
+
+  ru: Locale;
+
+  sk: Locale;
+
+  sl: Locale;
+
+  sq: Locale;
+
+  sr: Locale;
+
+  srLatn: Locale;
+
+  sv: Locale;
+
+  ta: Locale;
+
+  te: Locale;
+
+  th: Locale;
+
+  tr: Locale;
+
+  ug: Locale;
+
+  uk: Locale;
+
+  uz: Locale;
+
+  uzCyrl: Locale;
+
+  vi: Locale;
+
+  zhCN: Locale;
+
+  zhHK: Locale;
+  zhTW: Locale;
+}
+
+const loadPolyfillData = (lang: string) => {
+  //Load Lang polyfill
+  try {
+    require(`@formatjs/intl-displaynames/locale-data/${lang}`);
+  } catch (error) {}
+  try {
+    require(`@formatjs/intl-numberformat/locale-data/${lang}`);
+  } catch (error) {}
+  try {
+    require(`@formatjs/intl-listformat/locale-data/${lang}`);
+  } catch (error) {}
+
+  //Load Lang polyfill
+  try {
+    require(`@formatjs/intl-displaynames/locale-data/${lang.split('-')[0]}`);
+  } catch (error) {}
+  try {
+    require(`@formatjs/intl-numberformat/locale-data/${lang.split('-')[0]}`);
+  } catch (error) {}
+  try {
+    require(`@formatjs/intl-listformat/locale-data/${lang.split('-')[0]}`);
+  } catch (error) {}
+};
+
+export const locales: Locales = dateFNSLocaleFiles;
+
+export const rosetty = (
+  initialConfig: Record<string, Language>,
+  defaultLang?: string
+) => {
+  let config: Record<string, Language> = initialConfig;
+  let actualConfig: Language | undefined;
+  let actualLang: string | undefined;
+
+  if (typeof initialConfig !== 'object') {
+    throw new Error(
+      'rosetty: data must be an object with at least one language'
+    );
+  }
+
+  //Filter only languages who respect config format
+  config = Object.keys(config).reduce((acc: Record<string, Language>, lang) => {
+    if (
+      typeof config[lang] === 'object' &&
+      typeof config[lang].dict === 'object' &&
+      typeof config[lang].locale === 'object'
+    ) {
+      acc[lang] = config[lang];
+    }
+    return acc;
+  }, {});
+
+  const rosettaInstance = rosetta(
+    Object.entries(config)
+      ?.map(([key, value]) => [key, value.dict])
+      ?.reduce(
+        (prev, [key, value]) => ({ ...prev, [key as string]: value }),
+        {}
+      )
+  );
+
+  const changeLang = (lang: string) => {
+    const langConfig = config[lang];
+    if (!langConfig) {
+      throw new Error(`rosetty: language ${lang} not found`);
+    }
+
+    loadPolyfillData(lang);
+
+    actualConfig = config[lang];
+    actualLang = lang;
+    rosettaInstance.locale(lang);
+  };
+
+  if (defaultLang) {
+    changeLang(defaultLang);
+  }
+
+  return {
+    changeLang,
+    languages: Object.keys(config),
+    getCurrentLang: () => actualLang,
+    //Rosetta
+    t: (key: string, params?: Record<string, any>) =>
+      actualLang ? rosettaInstance.t(key, params) : undefined,
+    //Intl Polyfill
+    displayNames: (langCode: string, options: DisplayNamesOptions) => {
+      return new Intl.DisplayNames(
+        [actualConfig?.locale.code as string],
+        options
+      ).of(langCode);
+    },
+    listFormat: (list: string[], options: IntlListFormatOptions) => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      //@ts-ignore
+      return new Intl.ListFormat(actualConfig?.locale.code, options).format(
+        list
+      );
+    },
+    numberFormat: (value: number, options: NumberFormatOptions) => {
+      return new Intl.NumberFormat(actualConfig?.locale.code, options).format(
+        value
+      );
+    },
+    pluralRules: (
+      value: number,
+      options: {
+        type: 'cardinal' | 'ordinal';
+      }
+    ) => {
+      return new Intl.PluralRules(actualConfig?.locale.code, options).select(
+        value
+      );
+    },
+    //Date FNS i18n
+    format: (
+      date: number | Date,
+      stringFormat: string,
+      options: {
+        weekStartsOn?: 0 | 1 | 2 | 3 | 4 | 5 | 6;
+        firstWeekContainsDate?: number;
+        useAdditionalWeekYearTokens?: boolean;
+        useAdditionalDayOfYearTokens?: boolean;
+      } = {}
+    ) =>
+      format(date, stringFormat, { ...options, locale: actualConfig!.locale }),
+    formatRelative: (
+      date: number | Date,
+      baseDate: number | Date,
+      options: {
+        weekStartsOn?: 0 | 1 | 2 | 3 | 4 | 5 | 6;
+      } = {}
+    ) =>
+      formatRelative(date, baseDate, {
+        ...options,
+        locale: actualConfig!.locale,
+      }),
+    formatDistance: (
+      date: number | Date,
+      baseDate: number | Date,
+      options: {
+        includeSeconds?: boolean;
+        addSuffix?: boolean;
+      } = {}
+    ) =>
+      formatDistance(date, baseDate, {
+        ...options,
+        locale: actualConfig!.locale,
+      }),
+    formatDistanceToNow: (
+      date: number | Date,
+      options: {
+        includeSeconds?: boolean;
+        addSuffix?: boolean;
+      } = {}
+    ) =>
+      formatDistanceToNow(date, { ...options, locale: actualConfig!.locale }),
+    formatDuration: (
+      duration: Duration,
+      options: {
+        format?: string[];
+        zero?: boolean;
+        delimiter?: string;
+      } = {}
+    ) => formatDuration(duration, { ...options, locale: actualConfig!.locale }),
+  };
+};
---
+++ test/rosetty.test.ts
@@ -0,0 +1,371 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import {
+  format,
+  formatDistance,
+  formatDistanceToNow,
+  formatDuration,
+  formatRelative,
+  subDays,
+} from 'date-fns';
+
+import { locales, rosetty } from '../src';
+
+describe('rosetty', () => {
+  it('works', () => {
+    expect(rosetty).toBeDefined();
+  });
+
+  it('should return an error if no data is precise', () => {
+    expect(() => {
+      //@ts-ignore
+      return rosetty();
+    }).toThrowError(
+      'rosetty: data must be an object with at least one language'
+    );
+  });
+
+  describe('getCurrentLang', () => {
+    it('should return current lang', () => {
+      const { enGB: enLocale, fr: frLocale } = locales;
+      const r = rosetty(
+        {
+          en: {
+            dict: {},
+            locale: enLocale,
+          },
+          fr: {
+            dict: {},
+            locale: frLocale,
+          },
+        },
+        'en'
+      );
+
+      expect(r.getCurrentLang()).toBe('en');
+    });
+
+    it('should return undefined if no lang is defined', () => {
+      const { enGB: enLocale, fr: frLocale } = locales;
+      const r = rosetty({
+        en: {
+          dict: {},
+          locale: enLocale,
+        },
+        fr: {
+          dict: {},
+          locale: frLocale,
+        },
+      });
+
+      expect(r.getCurrentLang()).toBe(undefined);
+    });
+  });
+
+  describe('languages', () => {
+    it('should return only languages who is conform', () => {
+      const { enGB: enLocale } = locales;
+      const r = rosetty({
+        en: {
+          dict: {},
+          locale: enLocale,
+        },
+        //@ts-ignore
+        fr: {
+          dict: {},
+        },
+      });
+
+      expect(r.languages).toEqual(['en']);
+    });
+  });
+
+  describe('changeLang', () => {
+    it('should be able to change current lang', () => {
+      const { enGB: enLocale, fr: frLocale } = locales;
+      const r = rosetty(
+        {
+          en: {
+            dict: {},
+            locale: enLocale,
+          },
+          fr: {
+            dict: {},
+            locale: frLocale,
+          },
+        },
+        'en'
+      );
+
+      expect(r.getCurrentLang()).toBe('en');
+      r.changeLang('fr');
+      expect(r.getCurrentLang()).toBe('fr');
+      expect(r.languages).toEqual(['en', 'fr']);
+    });
+
+    it("should throw an error if lang doesn't exist", () => {
+      const { enGB: enLocale, fr: frLocale } = locales;
+      const r = rosetty(
+        {
+          en: {
+            dict: {},
+            locale: enLocale,
+          },
+          fr: {
+            dict: {},
+            locale: frLocale,
+          },
+        },
+        'en'
+      );
+
+      expect(() => {
+        r.changeLang('es');
+      }).toThrowError('rosetty: language es not found');
+    });
+  });
+
+  // Rosetta tests
+
+  describe('t', () => {
+    it('should return translated text', () => {
+      const { enGB: enLocale } = locales;
+      const r = rosetty(
+        {
+          en: {
+            dict: {
+              test: 'This is a test',
+            },
+            locale: enLocale,
+          },
+        },
+        'en'
+      );
+
+      expect(r.t('test')).toEqual('This is a test');
+    });
+
+    it('should return translated text with data', () => {
+      const { enGB: enLocale } = locales;
+      const r = rosetty(
+        {
+          en: {
+            dict: {
+              test: 'This is a {{firstName}}',
+            },
+            locale: enLocale,
+          },
+        },
+        'en'
+      );
+
+      expect(
+        r.t('test', {
+          firstName: 'test',
+        })
+      ).toEqual('This is a test');
+    });
+  });
+
+  // Format JS tests
+
+  describe('displayNames', () => {
+    it('should return same value as Intl', () => {
+      const { fr: frLocale } = locales;
+      const r = rosetty(
+        {
+          fr: {
+            dict: {},
+            locale: frLocale,
+          },
+        },
+        'fr'
+      );
+
+      expect(r.displayNames('fr', { type: 'language' })).toEqual(
+        new Intl.DisplayNames(['fr'], { type: 'language' }).of('fr')
+      );
+    });
+  });
+
+  describe('listFormat', () => {
+    it('should return same value as Intl', () => {
+      const { fr: frLocale } = locales;
+      const r = rosetty(
+        {
+          fr: {
+            dict: {},
+            locale: frLocale,
+          },
+        },
+        'fr'
+      );
+
+      expect(r.listFormat(['a', 'b', 'c'], { type: 'unit' })).toEqual(
+        //@ts-ignore
+        new Intl.ListFormat('fr', { type: 'unit' }).format(['a', 'b', 'c'])
+      );
+    });
+  });
+
+  describe('numberFormat', () => {
+    it('should return same value as Intl', () => {
+      const { fr: frLocale } = locales;
+      const r = rosetty(
+        {
+          fr: {
+            dict: {},
+            locale: frLocale,
+          },
+        },
+        'fr'
+      );
+
+      expect(
+        r.numberFormat(1230, {
+          style: 'currency',
+          currencySign: 'accounting',
+          currency: 'EUR',
+        })
+      ).toEqual(
+        //@ts-ignore
+        new Intl.NumberFormat('fr', {
+          style: 'currency',
+          currencySign: 'accounting',
+          currency: 'EUR',
+        }).format(1230)
+      );
+    });
+  });
+
+  describe('pluralRules', () => {
+    it('should return same value as Intl', () => {
+      const { fr: frLocale } = locales;
+      const r = rosetty(
+        {
+          fr: {
+            dict: {},
+            locale: frLocale,
+          },
+        },
+        'fr'
+      );
+
+      expect(r.pluralRules(2, { type: 'ordinal' })).toEqual(
+        //@ts-ignore
+        new Intl.PluralRules('fr', { type: 'ordinal' }).select(2)
+      );
+    });
+  });
+
+  // Date FNS tests
+  describe('format', () => {
+    it('should return same value as Date fns', () => {
+      const dateToTest = new Date();
+      const { fr: frLocale } = locales;
+      const r = rosetty(
+        {
+          fr: {
+            dict: {},
+            locale: frLocale,
+          },
+        },
+        'fr'
+      );
+
+      expect(r.format(dateToTest, 'yyyy-MM-dd')).toEqual(
+        format(dateToTest, 'yyyy-MM-dd', { locale: frLocale })
+      );
+    });
+  });
+
+  describe('formatRelative', () => {
+    it('should return same value as Date fns', () => {
+      const dateToTest = new Date();
+      const yesterdayDate = subDays(dateToTest, 1);
+      const { fr: frLocale } = locales;
+      const r = rosetty(
+        {
+          fr: {
+            dict: {},
+            locale: frLocale,
+          },
+        },
+        'fr'
+      );
+
+      expect(r.formatRelative(dateToTest, yesterdayDate)).toEqual(
+        formatRelative(dateToTest, yesterdayDate, { locale: frLocale })
+      );
+    });
+  });
+
+  describe('formatDistance', () => {
+    it('should return same value as Date fns', () => {
+      const dateToTest = new Date();
+      const yesterdayDate = subDays(dateToTest, 1);
+      const { fr: frLocale } = locales;
+      const r = rosetty(
+        {
+          fr: {
+            dict: {},
+            locale: frLocale,
+          },
+        },
+        'fr'
+      );
+
+      expect(r.formatDistance(dateToTest, yesterdayDate)).toEqual(
+        formatDistance(dateToTest, yesterdayDate, { locale: frLocale })
+      );
+    });
+  });
+
+  describe('formatDistanceToNow', () => {
+    it('should return same value as Date fns', () => {
+      const yesterdayDate = subDays(new Date(), 1);
+      const { fr: frLocale } = locales;
+      const r = rosetty(
+        {
+          fr: {
+            dict: {},
+            locale: frLocale,
+          },
+        },
+        'fr'
+      );
+
+      expect(r.formatDistanceToNow(yesterdayDate)).toEqual(
+        formatDistanceToNow(yesterdayDate, { locale: frLocale })
+      );
+    });
+  });
+
+  describe('formatDuration', () => {
+    it('should return same value as Date fns', () => {
+      const duration = {
+        years: 2,
+        months: 9,
+        weeks: 1,
+        days: 7,
+        hours: 5,
+        minutes: 9,
+        seconds: 30,
+      };
+
+      const { fr: frLocale } = locales;
+      const r = rosetty(
+        {
+          fr: {
+            dict: {},
+            locale: frLocale,
+          },
+        },
+        'fr'
+      );
+
+      expect(r.formatDuration(duration)).toEqual(
+        formatDuration(duration, { locale: frLocale })
+      );
+    });
+  });
+});

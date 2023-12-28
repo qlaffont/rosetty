08017e6a847c59d08b16a0acca7431df77e2da3f
Quentin <qlaffont13@gmail.com>
2023-12-28 08:19:00 +0100

    chore: fix build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,39 +1,16 @@
-// tsup.config.ts
-import { defineConfig } from 'tsup';
-import * as preset from 'tsup-preset-solid';
+import type { Options } from 'tsup';
 
-const preset_options: preset.PresetOptions = {
-  // array or single object
-  entries: [
-    // default entry (index)
-    {
-      // entries with '.tsx' extension will have `solid` export condition generated
-      entry: 'src/index.ts',
-    },
-  ],
-  // Set to `true` to remove all `console.*` calls and `debugger` statements in prod builds
-  drop_console: true,
-  // Set to `true` to generate a CommonJS build alongside ESM
-  cjs: true,
+export const tsup: Options = {
+  splitting: true,
+  clean: true, // clean up the dist folder
+  dts: true, // generate dts files
+  format: ['cjs', 'esm'], // generate cjs and esm files
+  minify: true,
+  bundle: true,
+  skipNodeModulesBundle: true,
+  entryPoints: ['src/index.ts'],
+  watch: false,
+  target: 'es2020',
+  outDir: 'dist',
+  entry: ['src/**/*.ts'], //include all files under src
 };
-
-export default defineConfig((config) => {
-  const watching = !!config.watch;
-
-  const parsed_data = preset.parsePresetOptions(preset_options, watching);
-
-  if (!watching) {
-    const package_fields = preset.generatePackageExports(parsed_data);
-
-    console.log(
-      `\npackage.json: \n${JSON.stringify(package_fields, null, 2)}\n\n`
-    );
-
-    /*
-        will update ./package.json with the correct export fields
-    */
-    preset.writePackageJson(package_fields);
-  }
-
-  return preset.generateTsupOptions(parsed_data);
-});
